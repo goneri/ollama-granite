4410129a5296ed753f6084a20587db78ba0b91e6
Gonéri Le Bouder <goneri@lebouder.net>
2025-04-11 17:14:22 -0400

    try to generate a role

--- main.py
+++ main.py
@@ -5,24 +5,42 @@ from pydantic import BaseModel
 
 from ollama import chat
 
-workspace = Path("/var/home/goneri/git_repos/wisdom/ansible-cline-mcp/workspace")
+workspace = Path(".")
 
 
 class ModelAnswer(BaseModel):
     file_name: str
-    final_answer: str|None
+    final_answer: str | None
+
 
 from ollama import Client
 
-question = [
-     {
+questions = [
+    {
         "role": "user",
         "content": f"What are the roles available in the directory {workspace}?",
     },
-
+    {
+        "role": "user",
+        "content": "Describe the variables to use to configure the first role in the list, remember that the roles are in the roles/ directory and you can request the file content",
+    },
+    {
+        "role": "user",
+        "content": "Describe the variables to use to configure the geerlingguy.redis role, remember that the roles are in the roles/ directory and you can request the file content",
+    },
+    {
+        "role": "user",
+        "content": "Describe the variables to use to configure the geerlingguy.docker role, remember that the roles are in the roles/ directory and you can request the file content",
+    },
+    {
+        "role": "user",
+        "content": "Prepare a playbook that uses the roles to install redis and docker on a host",
+    },
 ]
 
-messages = [
+question = questions.pop(0)
+
+system = [
     {
         "role": "system",
         "content": "You are an Ansible expert. "
@@ -30,37 +48,52 @@ messages = [
         "you use the key `file_name` to ask the name of the file or directory you want "
         "to read. The user will then have to give you the file content. "
         "The paths must me relative and remain inside the user workspace. "
-        "Keep the final_answer key set to None."
-        #"Once you've a good understanding of the user context, you can pass it through the final_answer key.",
+        "Keep the final_answer key set to None.",
+        # "Once you've a good understanding of the user context, you can pass it through the final_answer key.",
     },
 ]
-
 
 client = Client(
     host="http://localhost:11434",
 )
-response = client.chat(
-    model="granite3.2:8b",
-    format=ModelAnswer.model_json_schema(),
-    messages=messages + question,
-    options={
-        "num_ctx": 4096,
-    })
-while response:
-    print(f"response={response}")
-    if response.message.role != "assistant" or "file_name" not in response.message.content:
+
+
+conversation = []
+
+while True:
+    response = client.chat(
+        model="granite3.2:8b",
+        format=ModelAnswer.model_json_schema(),
+        messages=system + conversation + [question],
+        options={
+            "num_ctx": 4096,
+        },
+    )
+
+    # print(f"response={response}")
+    if (
+        response.message.role != "assistant"
+        or "file_name" not in response.message.content
+    ):
         break
+    conversation += [
+        {"role": response.message.role, "content": response.message.content}
+    ]
 
     model_answer = ModelAnswer.model_validate_json(response.message.content)
     if model_answer.final_answer:
+        print("----")
         print(model_answer.final_answer)
-        break
+        if questions == []:
+            exit(0)
+        question = questions.pop(0)
+        continue
+
     if not model_answer.file_name:
         break
 
-
     requested = workspace / model_answer.file_name
-    print(f"requested: {requested}")
+    print(f"filesystem access requested: {requested}")
     if not requested.exists():
         content = f"'{requested}' doesnt exist\n"
     elif requested.is_dir():
@@ -71,13 +104,4 @@ while response:
         content = f"this is the content of the file '{requested}':\n"
         content += requested.read_text()
 
-    messages.append({"role": "assistant", "content": content})
-    from pprint import pprint
-    pprint(messages)
-    response = client.chat(
-        model="granite3.2:8b",
-        format=ModelAnswer.model_json_schema(),
-        messages=messages + question,
-        options={
-            "num_ctx": 4096,
-        })
+    conversation.append({"role": "assistant", "content": content})
